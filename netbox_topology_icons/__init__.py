3b18f2e8e43990f91204200fbf638c4d2e75a0d6
Victor Gorchilov <victor@gorchilov.com>
2023-01-12 14:47:12 +0200

    [up] pdm

--- netbox_topology_icons/__init__.py
+++ netbox_topology_icons/__init__.py
@@ -1,11 +1,12 @@
 from extras.plugins import PluginConfig
 
+__version__ = "0.2.0"
 
 class TopoIconsConfig(PluginConfig):
     name = "netbox_topology_icons"
     verbose_name = "Topology icons"
     description = "Plugin to add icons to topology plugin"
-    version = "0.2.0"
+    version = __version__
     author = "Victor Gorchilov"
     author_email = "victor.gorchilov@x3me.net"
     base_url = ""
